50d1c5f772c3176ffacb00451a16379f3e11870b
Frankenmint <Frankenmint@users.noreply.github.com>
2016-03-20 04:58:44 -0700

    Added CSV Logging ✍️
    Added CSV Logging for Each Key
    
    Also added WIF conversion so it's easier to be imported

--- PkMaker.py
+++ PkMaker.py
@@ -5,10 +5,27 @@ import ecdsa
 import hashlib
 import base58
 import requests
-from time import sleep
+import time
+from smtplib import SMTP_SSL as SMTP
+import logging
+
+
+wif = ""
+
+
+
+logging.basicConfig(filename='BTC_PrivateKeys_'+time.strftime("%Y-%m-%d-%H-%M")+'.csv', \
+level=logging.INFO, format='%(message)s', datefmt='%Y-%m-%d,%H:%M:%S')
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.info ('"Timestamp", "WifKey", "PublicAddress"')
+
 
 
 def ping_address(publicAddress):
+	global pk
+	global wif
+	global publicKey
+
 	"""
 	sends Request to a Block Explorer	
 	Main one is blockexplorer - seems to be UNLIMITED...using chain.so has a rate limiter
@@ -22,13 +39,34 @@ def ping_address(publicAddress):
 	pmts = req.json()
 	balance =  pmts['balance']
 	print balance
+
+	# "WifKey", "HexKey", "PublicAddress", "PublicKey", "Balance"
+	logging.info (''+ time.strftime("%m-%d-%y %H:%M:%S") +','+ wif +','+publicAddress)
+
 	if float(balance) > 0.00000000:
 		print "Congratulations...alert the world cause you just made some sort of history friend!"
+
+
+def wif_conversion(pk):
+	global wif
+	padding = '80' + pk
+	# print padding
+
+	hashedVal = hashlib.sha256(padding.decode('hex')).hexdigest()
+	checksum = hashlib.sha256(hashedVal.decode('hex')).hexdigest()[:8]
+	# print hashedVal
+	# print padding+checksum
+
+	payload = padding + checksum
+	wif = base58.b58encode(payload.decode('hex'))
+	print wif
 	
 
 while True:
+
 	pk = os.urandom(32).encode("hex")
-	print pk
+	wif_conversion(pk)
+
 	sk = ecdsa.SigningKey.from_string(pk.decode("hex"), curve = ecdsa.SECP256k1)
 	vk = sk.verifying_key
 	publicKey = ("\04" + vk.to_string())
