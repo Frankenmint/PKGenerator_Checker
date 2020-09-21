794db996d4db74c8b82da037796fcbe6b2f044ba
Frankenmint <admin@honeybadgerofmoney.com>
2020-09-20 23:10:54 -0700

    initial commmit to bring PKMaker.py to py3

--- PkMaker.py
+++ PkMaker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-import os
+import secrets
+import json
+import codecs
 import ecdsa
 import hashlib
 import base58
@@ -8,7 +10,6 @@ import requests
 import time
 from smtplib import SMTP_SSL as SMTP
 import logging
-import blockcypher
 
 
 wif = ""
@@ -18,6 +19,7 @@ wif = ""
 logging.basicConfig(filename='BTC_PrivateKeys_'+time.strftime("%Y-%m-%d-%H-%M")+'.csv', \
 level=logging.INFO, format='%(message)s', datefmt='%Y-%m-%d,%H:%M:%S')
 logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("blockcypher").setLevel(logging.WARNING)
 logging.info ('"Timestamp", "WifKey", "PublicAddress"')
 
 
@@ -29,63 +31,75 @@ def ping_address(publicAddress):
 
 	"""
 	sends Request to a Block Explorer	
-	Use blockcypher Python package to fetch unlimited balances...
-	"""
 	
-	balance = blockcypher.get_total_balance(publicAddress)
-	print balance
+	NEEDS a good block explorer API to work with as blockcypher and blockchain.com have API limits.
+	"""
 
+	
+	resp = requests.get("https://blockstream.info/api/address/"+ publicAddress)
+
+	if resp.ok:
+		ourJSON = resp.json()
+		balance = dict(ourJSON['chain_stats'])['funded_txo_sum']
+		print( balance )
+
+	else:
+		print(resp.text)
+		raise ValueError("cannot reach block explorer for balance", resp)
 	# "WifKey", "HexKey", "PublicAddress", "PublicKey", "Balance"
 	#Comment out this line if you wish to NOT record blank keys
-	logging.info (''+ time.strftime("%m-%d-%y %H:%M:%S") +','+ wif +','+publicAddress)
+	logging.info (''+ time.strftime("%m-%d-%y %H:%M:%S") +','+ wif.decode('utf-8') +','+ publicAddress)
 
 	if float(balance) > 0.00000000:
-		logging.info (''+ time.strftime("%m-%d-%y %H:%M:%S") +','+ wif +','+publicAddress+' ,balance '+balance)
+		logging.info (''+ time.strftime("%m-%d-%y %H:%M:%S") +','+ wif.decode('utf-8') +','+publicAddress+' ,balance '+balance)
 		
-		print "Congratulations...alert the world cause you just made some sort of history friend!"
+		print( "Congratulations...alert the world cause you just made some sort of history friend!" )
+		print(wif.decode('utf-8'))
 
 
 def wif_conversion(pk):
 	global wif
 	padding = '80' + pk
-	# print padding
+	# print( padding )
 
-	hashedVal = hashlib.sha256(padding.decode('hex')).hexdigest()
-	checksum = hashlib.sha256(hashedVal.decode('hex')).hexdigest()[:8]
-	# print hashedVal
-	# print padding+checksum
+	hashedVal = hashlib.sha256(codecs.decode(padding, 'hex')).hexdigest()
+	checksum = hashlib.sha256(codecs.decode(hashedVal, 'hex')).hexdigest()[:8]
+	# print( hashedVal )
+	# print( padding+checksum )
 
 	payload = padding + checksum
-	wif = base58.b58encode(payload.decode('hex'))
-	print wif
+	wif = base58.b58encode(codecs.decode(payload, 'hex'))
+	print( wif.decode('utf-8') )
 	
 
 while True:
 
-	pk = os.urandom(32).encode("hex")
+	pk = secrets.token_hex(32)
 	wif_conversion(pk)
 
-	sk = ecdsa.SigningKey.from_string(pk.decode("hex"), curve = ecdsa.SECP256k1)
-	vk = sk.verifying_key
-	publicKey = ("\04" + vk.to_string())
+	sk = ecdsa.SigningKey.from_string(codecs.decode(pk, "hex"), curve=ecdsa.SECP256k1)
+	vk = sk.get_verifying_key()
+	publicKey = "\04" + str(vk.to_string())
 	ripemd160 = hashlib.new('ripemd160')
-	ripemd160.update(hashlib.sha256(publicKey).digest())
-	networkAppend = '\00' + ripemd160.digest()
+	ripemd160.update(hashlib.sha256(codecs.encode(publicKey)).digest())
+	networkAppend = b'\00' + ripemd160.digest()
 	checksum = hashlib.sha256(hashlib.sha256(networkAppend).digest()).digest()[:4]
 	binary_address = networkAppend + checksum
 	publicAddress = base58.b58encode(binary_address)
-	print publicAddress
+	print( publicAddress.decode('utf-8') )
 	while True:
 		try:
-			ping_address(publicAddress)	
+			ping_address(publicAddress.decode('utf-8'))
+			# probably does nothing...who knows ;)
+			time.sleep(.47)	
 		except ValueError:
-			print "Aaaannnnd we got Timed Out"
-			print pk
-			print publicAddress
+			print( "Aaaannnnd we got Timed Out" )
+			print( pk )
+			print( publicAddress )
 			time.sleep(3)
 			continue
 		except KeyError:
-			print "we may be denied or something, keep the script moving"
+			print( "we may be denied or something, keep the script moving" )
 			time.sleep(10)			
 		break
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # PKGenerator_Checker
-Generate Bitcoin Private Keys and check them against blockexplorer.com
+Generate Bitcoin Private Keys and check them against blockstreams liquid api.  I've tried blockcypher and blockexplorer.com but run into issues with limits that I'm FAAARRR to lazy to work around with a round robin of profile switches or setup a full archival node of my own to fire against. Feel free to fund my 
 
-Stupid Python Script that Generates random private keys and checks them in realtime...call this poor mans Mining for BTC - same chances as solo but you could find an address w/ money in it 🤑  This is for entertainment only and is designed to demonstrate how IMPOSSIBLE it is to realistically get a collision and take control of someone else's coin...Credits to @Shlomi for the inspiration...
+Stupid Python3 Script that Generates random private keys and checks them in realtime...call this poor mans Mining for BTC - same chances as solo but you could find an address w/ money in it 🤑  This is for entertainment only and is designed to demonstrate how IMPOSSIBLE it is to realistically get a collision and take control of someone else's coin...Credits to @Shlomi for the inspiration...
 
 # Instructions
 
@@ -17,7 +17,11 @@ Stupid Python Script that Generates random private keys and checks them in realt
 
 * What's Going on?:  A random 32 byte Number is generated and encoded into Hex - Basially a number between 1 and 2^256 OR if counting in decimal form: [115792089237316195423570985008687907853269984665640564039457584007913129639936](http://www.calculatorsoup.com/calculators/algebra/exponent.php).  Then, that key is hashed a few times into a public address [according to these standard rules](https://en.bitcoin.it/w/images/en/9/9b/PubKeyToAddr.png) and is fired off to blockexplorer.com using their API. The script then prints the balance to the console window.
 * I threw this together while following along [this video series](https://www.youtube.com/playlist?list=PLH4m2oS2ratfeNpZAoVwPlQqEr3HgNu7S) and reccomend YOU instead watch through the tutorials for your own benefit and to better grasp what happens at the protocol level for [Bitcoin](https://bitcoin.org)
-* I had to use cfscraper to get around the issue of cloudflare on the v2 version of the script which uses bitcoinlist.io this version will scan an entire page at a time of keys..though idk if the underlying site is to be trusted (ie they just tell you the funds are zero and sweep the funds into their own wallet first)
+
+
+~~* I had to use cfscraper to get around the issue of cloudflare on the v2 version of the script which uses bitcoinlist.io this version will scan an entire page at a time of keys..though idk if the underlying site is to be trusted (ie they just tell you the funds are zero and sweep the funds into their own wallet first)~~
+
+TODO: I believe the richlist is down right now but someone offerred a different page which I'll look intoat some point.
 
 Here's a demonstration of it in action
 
